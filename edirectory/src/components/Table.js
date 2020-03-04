babb9f171a98dd4376743de4f24b34fd5220b10b
mhitchi <mayhitchings@gmail.com>
2020-03-03 20:15:36 -0500

    tried inclass way for axios, no cigar, getting typeerror search is not a function

--- edirectory/src/components/Table.js
+++ edirectory/src/components/Table.js
@@ -1,33 +1,52 @@
 import React, { Component } from "react";
+import axios from "axios";
+import API from "../utils/API";
 
 
 class Table extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
+      err: null,
       isLoaded: false,
       items: []
     };
   }
 
   componentDidMount() {
-    fetch("https://randomuser.me/")
-      .then(res => JSON.parse(res))
-      .then(
-        result => {
-          this.setState({
-            isLoaded: true,
-            items: result.items
-          });
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    // fetch("https://randomuser.me/")
+    //   // TRIED .then(res => res.json())
+    //   // TRIED .then(res => JSON.parse())
+    //   .then(res => res.json())
+    //   .then(
+    //     result => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         items: result.items
+    //       });
+    //     },
+    //     error => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         error
+    //       });
+    //     }
+    //   )
+    this.getUsers();
+  }
+
+  //from lesson 19
+  getUsers = () => {
+    API.search()
+    .then(results => 
+      this.setState({
+        isLoaded: true,
+        items: results.items
+      }))
+      .catch(err => {
+        console.log(err);
+        this.setState({ err })
+      })
   }
 
   render() {
